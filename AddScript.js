a34f6ed5ae737b4e3185edf94d0ee0320bff12c0
mustafa2023w <121315397+mustafa2023w@users.noreply.github.com>
2022-12-24 01:19:15 +0500

    Add files via upload

---
+++ AddScript.js
@@ -0,0 +1,39 @@
+
+function generateProblem(){
+    var a = Math.floor((Math.random() * 10) + 1); 
+    var b = Math.floor((Math.random() * 10) + 1);
+    var op = '+'
+    var problem  = a + " " + op + " " + b 
+    document.getElementById("problem").innerHTML = problem 
+}
+
+
+function checkAnswer(){
+
+    var problem= document.getElementById("problem").innerHTML;
+    var correctAnswer=eval(problem);
+    var userAnswer=document.getElementById("answers").value ;
+    
+    if (userAnswer == correctAnswer){
+      alert("Correct Answer 😊");
+      generateProblem();
+      answers.value=''
+    }
+    else{
+      alert("Wrong Answer 🙁");
+      answers.value=''
+  
+  }
+ 
+}
+
+
+
+window.addEventListener("load", function(){
+  generateProblem();
+})
+
+ 
+
+
+  
---
+++ DivScript.js
@@ -0,0 +1,37 @@
+
+function generateProblem(){
+  var a = Math.floor((Math.random() * 10) + 1); 
+  var b = Math.floor((Math.random() * 10) + 1);
+  var op = '/'
+  var problem  = a + " " + op + " " + b 
+  document.getElementById("problem").innerHTML = problem 
+}
+
+
+function checkAnswer(){
+
+  var problem= document.getElementById("problem").innerHTML;
+  var correctAnswer=eval(problem);
+  var userAnswer=document.getElementById("answers").value ;
+  
+  if (userAnswer == correctAnswer){
+    alert("Great");
+    generateProblem();
+    answers.value=''
+  }
+  else{
+    alert("wrong");
+    answers.value=''
+
+}
+
+}
+
+window.addEventListener("load", function(){
+generateProblem();
+})
+
+
+
+
+
